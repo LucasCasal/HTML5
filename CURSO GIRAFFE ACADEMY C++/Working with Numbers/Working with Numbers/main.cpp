bf0f9bd5ca14265b0c3315197728d7bca359593a
Lucas Casal Goncalves Do Rego <dustwestcool@gmail.com>
2022-11-26 04:33:27 +0100

    CHANGES

---
+++ CURSO GIRAFFE ACADEMY C++/Working with Numbers/Working with Numbers/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << pow (3, 3) <<endl;
+    cout << sqrt (36) <<endl;
+    cout << round (4.3) <<endl;
+    cout << round (4.6) <<endl;
+    cout << ceil (4.1) <<endl;
+    cout << floor (4.8) <<endl;
+    cout << fmax (3, 10) <<endl;
+    cout << fmin (3, 10) <<endl;
+    cout << 5 * 7 <<endl;
+    cout << 5 / 7 <<endl;
+    cout << 40 <<endl;
+    cout << -408<<endl;
+    cout << 10 % 3<<endl;
+    cout << 4 + 5 * 10 <<endl;
+    cout << (4 + 5) * 10 <<endl;
+
+    int wnum = 5;
+    double dnum = 5.5;
+
+    wnum++;
+
+    cout << 10.0 / 3.0;
+
+
+    return 0;
+}
---
+++ CURSO VS CODE/src/readme.md
@@ -0,0 +1,10 @@
+Soy un readme probablemente no me leas
+pero realmente deberias hacerlo
+es un gran ejercicio para tu mente
+tambien para tus manos
+alimenta tu imaginacion
+deja que todo crezca con el tiempo
+que al final de la noche
+todo tendra su recompensa
+por eso mi querido amigo
+a programar se ha dicho
